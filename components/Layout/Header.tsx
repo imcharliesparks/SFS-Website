b917c245d90cc570cef33474feef30b5973be216
Charlie Sparks <sparksfullstack@gmail.com>
2023-01-02 16:27:55 -0500

    Charlie | Multiple updates made

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,17 +1,8 @@
-import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { NextAuthStatues } from '../../shared/types'
-import LoadingSpinner from '../general/LoadingSpinner'
 
 const Header = () => {
-	const { data: session, status } = useSession()
-	const handleSignOut = (event: React.MouseEvent<HTMLButtonElement>) => {
-		event.preventDefault()
-		signOut()
-	}
-
 	// TODO: Add hamburger menu
 	return (
 		<header>
@@ -22,57 +13,23 @@ const Header = () => {
 							<Image src={require('../../resources/sfs-logo.svg')} alt="The Sparks Full-Stack Logo" />
 						</Link>
 					</div>
-					<div className="hidden lg:flex space-x-6">
-						{session && status === NextAuthStatues.AUTHENTICATED ? (
-							<>
-								<Link href="/app/protected-page-example" className="hover:text-darkGrayishBlue transition-colors">
-									<span className=" hover:text-darkGrayishBlue transition-colors cursor-pointer">Protected</span>
-								</Link>
-								<Link href="/app/entity/create-entity" className="hover:text-darkGrayishBlue transition-colors">
-									<span className=" hover:text-darkGrayishBlue transition-colors cursor-pointer">Entities</span>
-								</Link>
-							</>
-						) : (
-							<>
-								<Link href="/sign-in" className="hover:text-darkGrayishBlue transition-colors">
-									<span className=" hover:text-darkGrayishBlue transition-colors cursor-pointer">Protected</span>
-								</Link>
-								<Link href="/sign-in">
-									<span className=" hover:text-darkGrayishBlue transition-colors cursor-pointer">Entities</span>
-								</Link>
-							</>
-						)}
-					</div>
 					<div className="flex flex-row">
-						{status === NextAuthStatues.LOADING ? (
-							<LoadingSpinner />
-						) : session && status === NextAuthStatues.AUTHENTICATED ? (
-							<button
-								onClick={handleSignOut}
-								className="hidden md:block bg-darkBlue px-6 py-2.5 rounded-full text-white baseline hover:bg-darkGrayishBlue transition-colors"
+						<Link href="/auth/sign-in">
+							<span
+								style={{ width: 106 }}
+								className="cursor-pointer hidden md:block px-6 py-2.5 rounded-full baseline hover:bg-sfs-blue hover:text-white transition-colors text-center mr-2"
 							>
-								Sign out
-							</button>
-						) : (
-							<>
-								<Link href="/auth/sign-in">
-									<span
-										style={{ width: 106 }}
-										className="cursor-pointer hidden md:block bg-darkBlue px-6 py-2.5 rounded-full text-white baseline hover:bg-darkGrayishBlue transition-colors text-center mr-2"
-									>
-										Sign In
-									</span>
-								</Link>
-								<Link href="/auth/sign-up">
-									<span
-										style={{ width: 106 }}
-										className="cursor-pointer hidden md:block bg-darkBlue px-6 py-2.5 rounded-full text-white baseline hover:bg-darkGrayishBlue transition-colors text-center"
-									>
-										Sign Up
-									</span>
-								</Link>
-							</>
-						)}
+								Sign In
+							</span>
+						</Link>
+						<Link href="/auth/sign-up">
+							<span
+								style={{ width: 106 }}
+								className="cursor-pointer hidden md:block px-6 py-2.5 rounded-full baseline hover:bg-sfs-blue hover:text-white transition-colors text-center"
+							>
+								Sign Up
+							</span>
+						</Link>
 					</div>
 				</div>
 			</nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,7 @@ module.exports = {
 							'dark': '#3c4858',
 							'black': '#161c2d',
 							'dark-footer': '#192132',
+							'sfs-blue': '#4F46E5'
 					},
 
 					boxShadow: {
